9f374cfb24319dddeaf10624ba0abcf5df9b9022
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2023-07-30 21:10:02 +0200

    changing bottom text after pressing DELETE

--- src/ui.rs
+++ src/ui.rs
@@ -14,6 +14,29 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
     // See the following resources:
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
     // - https://github.com/ratatui-org/ratatui/tree/master/examples
+    let vertical_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints(
+            [
+                Constraint::Percentage(10),
+                Constraint::Percentage(80),
+                Constraint::Percentage(10),
+            ]
+            .as_ref(),
+        )
+        .split(frame.size());
+    // "Welcome to amazing TODO app by prezes!"
+    let copyright = Paragraph::new(format!("{}", app.bottom_text))
+        .style(Style::default().fg(Color::White))
+        .alignment(Alignment::Center)
+        .block(
+            Block::default()
+                .borders(Borders::TOP)
+                .style(Style::default().fg(Color::White))
+                .border_type(BorderType::Plain),
+        );
+
+    frame.render_widget(copyright, vertical_chunks[2]);
     frame.render_widget(
         Paragraph::new(format!(
             "This is a tui template.\n\
@@ -29,34 +52,8 @@ pub fn render<B: Backend>(app: &mut App, frame: &mut Frame<'_, B>) {
                 .borders(Borders::ALL)
                 .border_type(BorderType::Rounded),
         )
-        .style(Style::default().fg(Color::Green).bg(Color::Black))
+        .style(Style::default().fg(Color::White).bg(Color::Black))
         .alignment(Alignment::Center),
         frame.size(),
     );
-
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(1)
-        .constraints(
-            [
-                Constraint::Percentage(10),
-                Constraint::Percentage(80),
-                Constraint::Percentage(10),
-            ]
-            .as_ref(),
-        )
-        .split(frame.size());
-
-    let copyright = Paragraph::new("pet-CLI 2020 - all rights reserved")
-        .style(Style::default().fg(Color::Green))
-        .alignment(Alignment::Center)
-        .block(
-            Block::default()
-                .borders(Borders::ALL)
-                .style(Style::default().fg(Color::Green))
-                .title("Copyright")
-                .border_type(BorderType::Plain),
-        );
-
-    frame.render_widget(copyright, chunks[1]);
 }
